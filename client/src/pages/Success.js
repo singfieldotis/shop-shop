b1f60b2a50404daec83526f9578aacb1021b98ce
Otis Singfield <singfieldotis@gmail.com>
2021-07-26 02:35:58 -0400

    added Stripe payment capability

---
+++ client/src/pages/Success.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useMutation } from "react";
+import Jumbotron from "../components/Jumbotron";
+import { ADD_ORDER } from "../utils/mutations";
+import { idbPromise } from "../utils/helpers";
+
+function Success() {
+  const [addOrder] = useMutation(ADD_ORDER);
+
+  useEffect(() => {
+    async function saveOrder() {
+      const cart = await idbPromise("cart", "get");
+      const products = cart.map((item) => item._id);
+
+      if (products.length) {
+        const { data } = await addOrder({ variables: { products } });
+        const productData = data.addOrder.products;
+
+        productData.forEach((item) => {
+          idbPromise('cart', 'delete', item);
+        });
+      }
+      setTimeout(() => {window.location.assign('/')}, 1000);
+    }
+
+    saveOrder();
+  }, [addOrder]);
+  return (
+    <div>
+      <Jumbotron>
+        <h1>Success!</h1>
+        <h2>Thank You for your purchase!</h2>
+        <h2>You will be redirected to the hompage in a few seconds.</h2>
+      </Jumbotron>
+    </div>
+  );
+}
+
+export default Success;
